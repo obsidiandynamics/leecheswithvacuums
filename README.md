4e3686d010175268fe7bd2b9348a08010924fa07
Emil Koutanov <ekoutanov@gmail.com>
2017-08-28 15:56:01 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# leecheswithvacuums
-Simple WebSocket throughput testing tool
+Leeches with Vacuums
+===
+Simple WebSocket throughput testing tool.
+
+# Usage
+Runs entirely through a [Gradle](https://github.com/gradle/gradle) task `runLWV`, passing in the required system properties.
+
+## Simple example
+```sh
+gradle runLWV -Dlwv.url="ws://localhost:8080/endpoint" -Dlwv.connections=100
+```
+Connects to the given URL using 100 connections, consuming messages as fast as the bandwidth allows.
+
+## Rate limiting
+```sh
+gradle runLWV -Dlwv.url="ws://localhost:8080/endpoint" -Dlwv.delay.min=5 -Dlwv.delay.max=10 -Dlwv.connections=100
+```
+Applies a varying degree of rate limiting - a random time penalty ranging from 5 ms per message (best case) to 10 ms (worst case).
+
+## Supported properties
+The following is a complete list of supported properties and their default values. All times are in milliseconds.
+```
+lwv.connections     : 1
+lwv.delay.max       : 0
+lwv.delay.min       : 0
+lwv.idleTimeout     : 600000
+lwv.reportInterval  : 5000
+lwv.url             : ws://localhost:8080
+```
---
+++ src/main/java/au/com/williamhill/leecheswithvacuums/LWV.java
@@ -0,0 +1,141 @@
+package au.com.williamhill.leecheswithvacuums;
+
+import static com.obsidiandynamics.indigo.util.PropertyUtils.*;
+
+import java.net.*;
+import java.nio.*;
+import java.util.*;
+import java.util.concurrent.atomic.*;
+
+import au.com.williamhill.flywheel.socketx.*;
+import au.com.williamhill.flywheel.socketx.undertow.*;
+
+public final class LWV extends Thread {
+  private static final Properties props = new Properties(System.getProperties());
+  private final String url = getOrSet(props, "lwv.url", String::valueOf, "ws://localhost:8080");
+  private final int connections = getOrSet(props, "lwv.connections", Integer::parseInt, 1);
+  private final int minDelayMillis = getOrSet(props, "lwv.delay.min", Integer::parseInt, 0);
+  private final int maxDelayMillis = getOrSet(props, "lwv.delay.max", Integer::parseInt, 0);
+  private final int reportIntervalMillis = getOrSet(props, "lwv.reportInterval", Integer::parseInt, 5_000);
+  private final int idleTimeoutMillis = getOrSet(props, "lwv.idleTimeout", Integer::parseInt, 600_000);
+  
+  private final long startTime = System.currentTimeMillis();
+  private long lastReportTime = startTime;
+  
+  private final AtomicLong received = new AtomicLong();
+  private long lastReceived;
+  
+  private LWV() throws Exception {
+    System.out.println("Leeches with Vacuums: starting...");
+    printProps();
+    
+    final XClient<UndertowEndpoint> client = UndertowClient.factory().create(new XClientConfig() {{
+      idleTimeoutMillis = LWV.this.idleTimeoutMillis;
+    }});
+    
+    for (int i = 0; i < connections; i++) {
+      connect(client);
+    }
+    
+    new Thread(this::reportLogger, "ReportLogger").start();
+  }
+  
+  private void printProps() {
+    final Map<Object, Object> sortedProps = new TreeMap<>();
+    filter("lwv.", props).entrySet().stream().forEach(e -> sortedProps.put(e.getKey(), e.getValue()));
+    sortedProps.entrySet().stream()
+    .map(e -> String.format("%-20s: %s", e.getKey(), e.getValue())).forEach(System.out::println);
+  }
+  
+  private int randomDelay() {
+    final int range = maxDelayMillis - minDelayMillis;
+    return (int) (Math.random() * range + minDelayMillis);
+  }
+  
+  private void connect(XClient<UndertowEndpoint> client) throws URISyntaxException, Exception {
+    client.connect(new URI(url), 
+                   new XEndpointListener<UndertowEndpoint>() {
+      @Override
+      public void onConnect(UndertowEndpoint endpoint) {
+        System.out.println("Connected to " + endpoint);
+      }
+
+      @Override
+      public void onText(UndertowEndpoint endpoint, String message) {
+        handleMessage(endpoint);
+      }
+
+      @Override
+      public void onBinary(UndertowEndpoint endpoint, ByteBuffer message) {
+        handleMessage(endpoint);
+      }
+
+      @Override
+      public void onPing(UndertowEndpoint endpoint, ByteBuffer data) {}
+
+      @Override
+      public void onPong(UndertowEndpoint endpoint, ByteBuffer data) {}
+
+      @Override
+      public void onDisconnect(UndertowEndpoint endpoint, int statusCode, String reason) {
+        System.out.println("Disconnected from " + endpoint + " statusCode=" + statusCode + " reason=" + reason);
+      }
+
+      @Override
+      public void onClose(UndertowEndpoint endpoint) {
+        System.out.println("Closed " + endpoint);
+      }
+
+      @Override
+      public void onError(UndertowEndpoint endpoint, Throwable cause) {
+        System.out.println("Error on endpoint " + endpoint + ": " + cause);
+      }
+    });
+  }
+  
+  private void handleMessage(UndertowEndpoint endpoint) {
+    final int delayMillis = randomDelay();
+    if (delayMillis != 0) {
+      endpoint.getChannel().suspendReceives();
+      new Thread(() -> {
+        try {
+          Thread.sleep(delayMillis);
+        } catch (InterruptedException e) {
+          return;
+        } finally {
+          endpoint.getChannel().resumeReceives();
+        }
+      }, "ReceiveResumer").start();
+    }
+    received.incrementAndGet();
+  }
+  
+  private void reportLogger() {
+    for (;;) {
+      try {
+        Thread.sleep(reportIntervalMillis);
+      } catch (InterruptedException e) {
+        return;
+      }
+      
+      final long now = System.currentTimeMillis();
+      final long totalReceived = received.get();
+      final long totalElapsed = now - startTime;
+      final long reportReceived = totalReceived - lastReceived;
+      final long reportElapsed = now - lastReportTime;
+      
+      final double averageRate = 1000d * totalReceived / totalElapsed;
+      final double currentRate = 1000d * reportReceived / reportElapsed;
+      
+      System.out.format("%,d messages; average rate: %,.0f msg/s, current rate: %,.0f msg/s\n",
+                        totalReceived, averageRate, currentRate);
+      
+      lastReportTime = now;
+      lastReceived = totalReceived;
+    }
+  }
+  
+  public static void main(String[] args) throws Exception {
+    new LWV();
+  }
+}
